631937fb07f8fb0fba881018b4cb2ae049ec81e1
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-12-01 14:21:52 +0900

    feat: ポケモンタイプごとの色の設定

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,4 +1,79 @@
 import 'dart:ui';
 
 const PokemonRed = Color.fromRGBO(228, 11, 32, 1);
-const normalTypeColor = Color.fromRGBO(159, 161, 159, 1);
+
+const normalTypeColor = Color(0xff9fa19f);
+const normalTypeDarkColor = Color(0xff676967);
+const normalTypeLightColor = Color(0xffc1c2c1);
+
+const fightingTypeColor = Color(0xffc03028);
+const fightingTypeDarkColor = Color(0xff7d1f1a);
+const fightingTypeLightColor = Color(0xffd67873);
+
+const flyingTypeColor = Color(0xffa890f0);
+const flyingTypeDarkColor = Color(0xff6d5e9c);
+const flyingTypeLightColor = Color(0xffc6b7f5);
+
+const poisonTypeColor = Color(0xffa040a0);
+const poisonTypeDarkColor = Color(0xff682a68);
+const poisonTypeLightColor = Color(0xffc183c1);
+
+const groundTypeColor = Color(0xffe0c068);
+const groundTypeDarkColor = Color(0xff927d44);
+const groundTypeLightColor = Color(0xffebd69d);
+
+const rockTypeColor = Color(0xffb8a038);
+const rockTypeDarkColor = Color(0xff786824);
+const rockTypeLightColor = Color(0xffd1c17d);
+
+const bugTypeColor = Color(0xffa8b820);
+const bugTypeDarkColor = Color(0xff6d7815);
+const bugTypeLightColor = Color(0xffc6d16e);
+
+const ghostTypeColor = Color(0xff705898);
+const ghostTypeDarkColor = Color(0xff493963);
+const ghostTypeLightColor = Color(0xffa292bc);
+
+const steelTypeColor = Color(0xffb8b8d0);
+const steelTypeDarkColor = Color(0xff787887);
+const steelTypeLightColor = Color(0xffd1d1e0);
+
+const fireTypeColor = Color(0xfff08030);
+const fireTypeDarkColor = Color(0xff9c531f);
+const fireTypeLightColor = Color(0xfff5ac78);
+
+const waterTypeColor = Color(0xff6890f0);
+const waterTypeDarkColor = Color(0xff445e9c);
+const waterTypeLightColor = Color(0xff9db7f5);
+
+const grassTypeColor = Color(0xff78c850);
+const grassTypeDarkColor = Color(0xff4e8234);
+const grassTypeLightColor = Color(0xffa7db8d);
+
+const electricTypeColor = Color(0xfff8d030);
+const electricTypeDarkColor = Color(0xffa1871f);
+const electricTypeLightColor = Color(0xfffae078);
+
+const psychicTypeColor = Color(0xfff85888);
+const psychicTypeDarkColor = Color(0xffa13959);
+const psychicTypeLightColor = Color(0xfffa92b2);
+
+const iceTypeColor = Color(0xff98d8d8);
+const iceTypeDarkColor = Color(0xff638d8d);
+const iceTypeLightColor = Color(0xffbce6e6);
+
+const dragonTypeColor = Color(0xff7038f8);
+const dragonTypeDarkColor = Color(0xff4924a1);
+const dragonTypeLightColor = Color(0xffa27dfa);
+
+const darkTypeColor = Color(0xff705848);
+const darkTypeDarkColor = Color(0xff49392f);
+const darkTypeLightColor = Color(0xffa29288);
+
+const fairyTypeColor = Color(0xffef70ef);
+const fairyTypeDarkColor = Color(0xff9b499b);
+const fairyTypeLightColor = Color(0xfff5a2f5);
+
+const stellarTypeColor = Color(0xff7cc7b2);
+const stellarTypeDarkColor = Color(0xff517280);
+const stellarTypeLightColor = Color(0xffe2efd3);
